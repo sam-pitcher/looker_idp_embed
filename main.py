64f138eab8f3d3f6a204547ae31d36e9da55895a
Sam Pitcher <sampitcher@google.com>
2023-04-06 12:46:00 +0200

    added lookml dashboard

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from flask import Flask, redirect, request, url_for, render_template
 
 from looker_api import looker_create_sso_url_as_me as looker_create_sso_url_as_me, looker_search_user as looker_search_user
 
-LOOKER_DASHBOARD_ID = '6'
+LOOKER_DASHBOARD_ID = '8'
 LOOKER_THEME = 'Test'
+LOOKER_MODEL_NAME = 'sam-pitcher-playground'
+LOOKERSDK_BASE_URL = 'https://test.looker.shredr.xyz'
 
 # Flask app setup
 app = Flask(__name__)
@@ -18,14 +20,14 @@ app.secret_key = os.urandom(24)
 @app.route("/index")
 def index():
     user_email = 'sampitcher@google.com'
-    dashboard_url = "https://test.looker.shredr.xyz/embed/dashboards/{LOOKER_DASHBOARD_ID}"
+    dashboard_url = f"{LOOKERSDK_BASE_URL}/embed/dashboards/{LOOKER_DASHBOARD_ID}"
     try:
         user = looker_search_user(user_email)
         user_id = user.id
         url_response = looker_create_sso_url_as_me(dashboard_url, user_id)
         tile_url_1 = url_response.url
     except:
-        tile_url_1 = dashboard_url
+        tile_url_1 = f"{LOOKERSDK_BASE_URL}/embed/dashboards/{LOOKER_MODEL_NAME}::redirect?allow_login_screen=true"
 
     print(tile_url_1)
 
